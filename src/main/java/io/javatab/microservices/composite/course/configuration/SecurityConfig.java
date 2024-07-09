39f84a2a354cdd71e39a6f689ff8dd92e62c151f
Pooja <149813908+sakthivarthini@users.noreply.github.com>
2024-07-09 11:15:11 +0530

    Update SecurityConfig.java

--- src/main/java/io/javatab/microservices/composite/course/configuration/SecurityConfig.java
+++ src/main/java/io/javatab/microservices/composite/course/configuration/SecurityConfig.java
@@ -4,13 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.beans.factory.annotation.Value;
 
 import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        return NimbusReactiveJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
+    }
 
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
